80facd6dfeb8e6d5fed9898fffbe7fdac947592a
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2020-08-18 12:57:24 +0200

    refs #3809: replace what is ratenkauf by easycredit link; updated compatibility

--- src/woocommerce-gateway-ratenkaufbyeasycredit/woocommerce-gateway-ratenkaufbyeasycredit.php
+++ src/woocommerce-gateway-ratenkaufbyeasycredit/woocommerce-gateway-ratenkaufbyeasycredit.php
@@ -9,16 +9,16 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     woocommerce-gateway-ratenkaufbyeasycredit
  * Domain Path:     /languages
- * Version:         1.6.8
+ * Version:         1.6.9
  * WC requires at least: 3.0.0
- * WC tested up to: 4.3.1
+ * WC tested up to: 4.3.3
  *
  * @package         Woocommerce_Gateway_Ratenkaufbyeasycredit
  */
 
 defined( 'ABSPATH' ) or exit;
 
-define( 'WC_RATENKAUFBYEASYCREDIT_VERSION', '1.6.8' );
+define( 'WC_RATENKAUFBYEASYCREDIT_VERSION', '1.6.9' );
 define( 'WC_RATENKAUFBYEASYCREDIT_ID', 'ratenkaufbyeasycredit' );
 
 function wc_ratenkaufbyeasycredit() {
