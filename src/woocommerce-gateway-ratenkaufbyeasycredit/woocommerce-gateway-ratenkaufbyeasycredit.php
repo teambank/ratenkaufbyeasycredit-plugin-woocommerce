b02a80f579d8d6ec06113517bd96fb422dc7ee31
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2020-09-14 17:17:56 +0200

    version up, added changelog

--- src/woocommerce-gateway-ratenkaufbyeasycredit/woocommerce-gateway-ratenkaufbyeasycredit.php
+++ src/woocommerce-gateway-ratenkaufbyeasycredit/woocommerce-gateway-ratenkaufbyeasycredit.php
@@ -9,7 +9,7 @@
  * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:     woocommerce-gateway-ratenkaufbyeasycredit
  * Domain Path:     /languages
- * Version:         1.6.9
+ * Version:         1.6.10
  * WC requires at least: 3.0.0
  * WC tested up to: 4.5.1
  *
@@ -18,7 +18,7 @@
 
 defined( 'ABSPATH' ) or exit;
 
-define( 'WC_RATENKAUFBYEASYCREDIT_VERSION', '1.6.9' );
+define( 'WC_RATENKAUFBYEASYCREDIT_VERSION', '1.6.10' );
 define( 'WC_RATENKAUFBYEASYCREDIT_ID', 'ratenkaufbyeasycredit' );
 
 function wc_ratenkaufbyeasycredit() {
